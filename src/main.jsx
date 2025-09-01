9d11d5e9cfc4011058e06f11aa5a86b68766aaa7
Nicolas Rodriguez <NRodriguez@CTS.LOCAL>
2025-09-01 12:34:45 -0300

    fix main theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components';
-import { theme } from "./styles/Theme";
+import { theme } from "./styles/theme";
 
 import App from './App.jsx'
 
